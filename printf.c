559f4647b5ef3de508ba94251f47e0a47c598d7c
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2023-09-16 23:44:12 +0100

    bbb

---
+++ printf.c
@@ -0,0 +1,41 @@
+#include "main.h"
+int _printf(const char *format, ...)
+{
+	int i, count = 0;
+	va_list args;
+
+	va_start(args, format);
+	if (_testFormat(format) == -1)
+	{
+		va_end(args);
+		return (-1);
+	}
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			write(1, &format[i], 1);
+			count++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 's')
+			{
+				count = testeStr(count, args);
+			}
+			else if (format[i] == 'c')
+			{
+				count = testChar(count, args);
+			}
+			else if (format[i] == '%')
+			{
+				count = teste(count);
+			{
+		}
+	}
+
+	va_end(args);
+	return (count);
+}
