dfb864d3e023eae0e6a18f7da84db8bdfbd48e08
hamazoon <hamazoon1@gmail.com>
2023-09-17 23:15:48 +0100

    ibts

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int teste(int count);
+int testChar(int count, va_list args);
+int testeStr(int count, va_list args);
+int _testFormat(const char *format);
+#endif /* MAIN_H */
