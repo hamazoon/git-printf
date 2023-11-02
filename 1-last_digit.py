95aa395cc129d264cb050be6a594cebbca18199c
hamazoon <hamazoon1@gmail.com>
2023-11-02 01:31:03 +0100

    boussi ida kan da sa7i7

--- 1-last_digit.py
+++ 1-last_digit.py
@@ -8,6 +8,6 @@ print("last digit of {} is {}".format(number,digit),end="")
 if digit > 5:
     print(" and is greater than 5")
 elif digit == 0:
-    print("0")
+    print(" and is 0")
 else:
     print(" and is less than 6 and not 0")
